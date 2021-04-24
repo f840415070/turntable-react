62b7b6c646ab84adba9be1d240c42348ff6ff89a
fanyibin <f84041507@163.com>
2021-04-24 20:51:17 +0800

    feat: 绘制图片

--- lib/turntable.tsx
+++ lib/turntable.tsx
@@ -1,18 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { FC, useEffect } from 'react';
 import Controller from './controller';
 
-function Turntable({ size, prizes }: TurntableTypes.Props) {
-  const controller = new Controller(size, prizes);
+const Turntable: FC<TurntableTypes.Props> = (props: TurntableTypes.Props) => {
+  const {
+    size,
+    prizes,
+    children,
+    onPress,
+  } = props;
+
+  let controller;
   useEffect(() => {
-    console.log('转盘开始渲染');
+    controller = new Controller(size, prizes);
     controller.init();
+    console.log('转盘渲染完成');
   }, []);
 
+  // 启动抽奖
+  const start = () => {
+    onPress()
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(() => {});
+  };
+
   return (
-    <canvas id="__turntable" width={size} height={size}>
-      You need to update your browser because canvas is not supported.
-    </canvas>
+    <div
+      className="__turntable-container"
+      style={{ position: 'relative' }}
+    >
+      <canvas id="__turntable-canvas" width={size} height={size}>
+        You need to update your browser to support canvas.
+      </canvas>
+      <div
+        className="__turntable-over-box"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          overflow: 'visible',
+        }}
+      >
+        <div className="__turntable-pointer" onClick={start}>
+          {children}
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Turntable;

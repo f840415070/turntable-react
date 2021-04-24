d0e86313dc50e04835c7a0e749da7a607745be6c
fanyibin <f84041507@163.com>
2021-04-24 22:53:39 +0800

    feat: add pointer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
 import Turntable from '../lib/turntable';
+import './App.css';
+
+const testColors: string[] = [
+  '#62CDD8', '#FFFFFF', '#FEB446', '#FFFFFF', '#62CDD8', '#FFFFFF', '#FEB446', '#FFFFFF',
+];
+const testPrizes: TurntableTypes.Prize[] = Array(8).fill(0).map((_, index) => ({
+  title: index === 0 ? '谢谢参与' : `${(index + 1) * 1000}元大奖`,
+  backgroundColor: testColors[index],
+  fontStyle: '16px Arial',
+  image: index === 0 ? null : ({
+    src: '../src/gift.png',
+    width: 30,
+    height: 30,
+  }),
+}));
 
 function App() {
-  const testColor: string[] = ['red', 'orange', 'yellow', 'green', 'gray', '#6edbe6', 'purple', '#AED54C'];
-  const prizes: TurntableTypes.Prize[] = Array(8).fill(0).map((_, index) => ({
-    title: `${(index + 1) * 1000}元大奖`,
-    backgroundColor: testColor[index],
-    fontStyle: '14px',
-    image: {
-      src: '../src/gift.png',
-      width: 30,
-      height: 30,
-    },
-  }));
+  const getPrizeResult = () => {
+    return new Promise<number>((resolve, reject) => {
+      const testResult = Math.floor(Math.random() * 8);
+      if (testResult >= 7) { // 未达条件不启动抽奖
+        reject();
+      }
+      // setTimeout 模拟接口请求结果
+      setTimeout(() => {
+        resolve(testResult);
+      }, 50);
+    });
+  };
 
   return (
-    <div
-      className="turntable-container"
-      style={{ width: 360, height: 360, margin: 'auto' }}
-    >
-      <Turntable size={360} prizes={prizes} />
+    <div className="turntable">
+      <Turntable
+        size={360}
+        prizes={testPrizes}
+        onPress={getPrizeResult}
+        renderAfterImagesLoaded={true}
+      >
+        {/* 转盘指针 点击按钮 */}
+        <div className="turntable-pointer">
+          <img className="pointer-img" src={require('./pointer.png')} alt="" />
+        </div>
+      </Turntable>
     </div>
   );
 }
